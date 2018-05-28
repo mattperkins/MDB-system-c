f3b2f6d578230129320e6432959517f5836dcadd
mattperkins <github@mattperkins.com>
2018-05-28 17:24:25 +0100

    configure and run test

---
+++ test/mocha_test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+// Describe tests
+describe('a test', function(){
+
+    // Create test
+    it('adds two numbers together', function(){
+        assert(2 + 3 === 5);
+    });
+
+});
