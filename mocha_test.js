ce2822701f30f83e47b1074180e840932f453b93
mattperkins <github@mattperkins.com>
2018-05-28 16:18:59 +0100

    add test

---
+++ mocha_test.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+
+// Describe tests
+describe('a test', ()=>{
+
+    // Create test
+    it('add two numbers together', ()=>{
+        assert(2 + 3 === 5)
+    })
+
+})
